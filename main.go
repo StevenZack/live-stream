f06127cb14513817042b3575c3c741f505ff0c7c
StevenZack <stevenzack@qq.com>
2017-12-20 16:14:25 +0800

    finished

--- main.go
+++ main.go
@@ -9,10 +9,6 @@ import (
 
 var members []*websocket.Conn
 
-type Msg struct {
-	Data, MType string
-}
-
 func main() {
 	http.Handle("/chat", websocket.Handler(chat))
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./public"))))
@@ -28,11 +24,14 @@ func chat(ws *websocket.Conn) {
 		if e := websocket.Message.Receive(ws, &str); e != nil {
 			break
 		}
-		for k, v := range members {
-			if e := websocket.Message.Send(v, str); e != nil {
-				members = append(members[:k], members[k+1:]...) //delete offline Conn in member
-				break
-			}
+		go sendToAll(str)
+	}
+}
+func sendToAll(str string) {
+	for k, v := range members {
+		if e := websocket.Message.Send(v, str); e != nil {
+			members = append(members[:k], members[k+1:]...) //delete offline Conn in member
+			break
 		}
 	}
 }
