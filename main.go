616563c96940e97fb2915b832f50025ae5e1caea
StevenZack <stevenzack@qq.com>
2017-12-20 15:58:04 +0800

    Just a backup

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+var members []*websocket.Conn
+
+type Msg struct {
+	Data, MType string
+}
+
+func main() {
+	http.Handle("/chat", websocket.Handler(chat))
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./public"))))
+	if e := http.ListenAndServe(":9090", nil); e != nil {
+		fmt.Println(e)
+	}
+}
+func chat(ws *websocket.Conn) {
+	defer ws.Close()
+	members = append(members, ws)
+	for {
+		str := ""
+		if e := websocket.Message.Receive(ws, &str); e != nil {
+			break
+		}
+		for k, v := range members {
+			if e := websocket.Message.Send(v, str); e != nil {
+				members = append(members[:k], members[k+1:]...) //delete offline Conn in member
+				break
+			}
+		}
+	}
+}
